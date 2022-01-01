99d332ef890ba408cc0e3f77b0833ea1f7cc1e77
maxProgrammer <maxwneto.poo@gmail.com>
2022-01-01 13:14:52 -0300

    interface to make a contract of taxservice

---
+++ src/com/maxprogrammer/entities/UsaInterestService.java
@@ -0,0 +1,17 @@
+package com.maxprogrammer.entities;
+
+import java.security.InvalidParameterException;
+
+public class UsaInterestService implements InterestService {
+    private double interestRate;
+
+    public UsaInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+}
